c0f16aa8f69bfe775dfb61b642647c5036ab0e0c
KorOnechild <kyuhong99@gmail.com>
2022-11-25 19:00:49 +0900

    [3-2] DetailPage 구현 강의본

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import MainPage from './pages/MainPage';
 import AboutPage from './pages/AboutPage';
+import DetailPage from './pages/DetailPage';
 
 export default function App() {
   return (
     // <MainPage/>
-    <AboutPage/>
+    // <AboutPage/>
+    <DetailPage/>
   );
 }
