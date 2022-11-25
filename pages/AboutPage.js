839ca2bf49a6b80bf67a31df114b55c38814f149
KorOnechild <kyuhong99@gmail.com>
2022-11-25 18:38:55 +0900

    [2-7] AboutPage 제작 강의본 코드

--- pages/AboutPage.js
+++ pages/AboutPage.js
@@ -1,80 +1,76 @@
 import React from 'react'
-import { StyleSheet, View, Text, Image, TouchableOpacity } from 'react-native'
+import {View,Text,StyleSheet,Image, TouchableOpacity} from 'react-native'
 
-export default function AboutPage() {
-    const mainImage = 'https://storage.googleapis.com/sparta-image.appspot.com/lecture/about.png'
 
-    return (
-        <View style={styles.container}>
-            <Text style={styles.title}>HI! 스파르타코딩 앱개발 반에 오신것을 환영합니다</Text>
-            <View style={styles.contentBox}>
-                <Image 
-                    source={{uri:mainImage}}
-                    style={styles.mainImage}
-                    resizeMode={'cover'}
-                />
-                <Text style={styles.contentTitle}> 많은 내용을 간결하게 담아내려 노력했습니다!</Text>
-                <Text style={styles.contentText}> 꼭 완주 하셔서 꼭 여러분것으로 만들어가시길 바랍니다</Text>
-                <TouchableOpacity style={styles.touchBox}>
-                    <Text style={styles.touchBox.text}>여러분의 인스타계정</Text>
-                </TouchableOpacity>
-            </View>
+export default function AboutPage(){
+    const aboutImage = "https://storage.googleapis.com/sparta-image.appspot.com/lecture/about.png"
+  return (
+    <View style={styles.container}>
+        <Text style={styles.title}>HI! 스파르타코딩 앱개발 반에 오신것을 환영합니다</Text>
+       
+        
+        <View style={styles.textContainer}>
+            <Image style={styles.aboutImage} source={{uri:aboutImage}} resizeMode={"cover"}/>
+            <Text style={styles.desc01}>많은 내용을 간결하게 담아내려 노력했습니다!</Text>
+            <Text style={styles.desc02}>꼭 완주 하셔서 꼭 여러분것으로 만들어가시길 바랍니다</Text>
+            <TouchableOpacity style={styles.button}>
+                <Text style={styles.buttonText}>여러분의 인스타계정</Text>
+            </TouchableOpacity>
         </View>
-    )
+    </View>)
 }
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
-        backgroundColor: 'rgb(38, 44, 116)'
+        flex:1,
+        backgroundColor:"#1F266A",
+        alignItems:"center"
+      
     },
-    contentBox: {
-        flex: 1,
-        justifyContent: 'center',
-        margin: 40,
-        marginBottom: 80,
-        paddingLeft: 20,
-        paddingRight: 20, 
-        backgroundColor: '#fff',
-        borderRadius: 30
-    },
-    mainImage:{
-        alignSelf: 'center',
-        width: '60%',
-        height: '30%',
-        borderRadius: 30
-    },  
     title: {
-        fontSize: 30,
-        marginTop: 90,
-        marginLeft: 30,
-        marginRight: 30,
-        marginBottom: 20,
-        fontWeight: '700',
-        textAlign: 'center',
-        color: 'white'
+        fontSize:30,
+        fontWeight:"700",
+        color:"#fff",
+        paddingLeft:30,
+        paddingTop:100,
+        paddingRight:30
     },
-    contentTitle: {
-        fontSize: 20,
-        fontWeight: '700',
-        textAlign: 'center',
-        marginBottom: 20 
+    textContainer: {
+        width:300,
+        height:500,
+        backgroundColor:"#fff",
+        marginTop:50,
+        borderRadius:30,
+        justifyContent:"center",
+        alignItems:"center"
     },
-    contentText:{
-        fontSize: 15,
-        fontWeight: '700',
-        textAlign: 'center',
-        marginBottom: 20 
+    aboutImage:{
+        width:150,
+        height:150,
+        borderRadius:30
     },
-    touchBox: {
-        backgroundColor: 'orange',
-        borderRadius: 20,
-        padding: 20,
-        alignSelf: 'center',
+    desc01: {
+        textAlign:"center",
+        fontSize:20,
+        fontWeight:"700",
+        paddingLeft:22,
+        paddingRight:22
 
-        text: {
-            color: '#fff',
-            fontWeight: '700'
-        }
     },
+    desc02: {
+        textAlign:"center",
+        fontSize:15,
+        fontWeight:"700",
+        padding:22
+    },
+    button:{
+        backgroundColor:"orange",
+        padding:20,
+        borderRadius:15
+    },
+    buttonText: {
+        color:"#fff",
+        fontSize:15,
+        fontWeight:"700"
+    }
 })
