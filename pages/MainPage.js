6907f977b7b4d5fa17af0dc9054c26187e367227
KorOnechild <kyuhong99@gmail.com>
2022-11-26 15:23:38 +0900

    [3-5] 데이터를 가지고 페이지 이동하기
    - navigation.navigate()를 사용해 이동할 페이지 지정과 가지고갈 데이터를 넘겨주고, 받는 페이지에서 route.params에서 받은 객체를 꺼내서 사용
    - Stack.Screen에서 지정하지 않은 페이지도 navigation 객체를 사용할 수 있도록 해당 컴포넌트에 'navigation' 키값으로 속성을 넘겨준다.

--- pages/MainPage.js
+++ pages/MainPage.js
@@ -21,10 +21,11 @@ export default function MainPage({navigation,route}) {
   const [ready,setReady] = useState(true)
 
   useEffect(()=>{
-	   
+
 		//뒤의 1000 숫자는 1초를 뜻함
     //1초 뒤에 실행되는 코드들이 담겨 있는 함수
     setTimeout(()=>{
+        
         //헤더의 타이틀 변경
         navigation.setOptions({
           title:'나만의 꿀팁'
